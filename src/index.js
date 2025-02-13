b48ed739e0acd1ef14ef0c50a4ee5084079fd00c
rajeev <rajeevkumar25112002@gmail.com>
2025-02-13 16:14:50 +0530

    initialize project structure with database connection and configuration

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** 
+
+const app = express()
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log(`express error:${error}`);
+            throw error
+            
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.log(`mongodb connection error:${error}`);
+        throw error
+        
+        
+    }
+})()
+*/
